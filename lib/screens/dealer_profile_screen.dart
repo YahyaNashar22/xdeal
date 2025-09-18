75ad44248c912b4b9e6324f6c8fe18a53b83956e
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-09-18 12:43:32 +0300

    added inital dealer profile info

--- lib/screens/dealer_profile_screen.dart
+++ lib/screens/dealer_profile_screen.dart
@@ -66,7 +66,64 @@ class _DealerProfileScreenState extends State<DealerProfileScreen> {
           ),
         ],
       ),
-      body: SafeArea(child: SingleChildScrollView()),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.network(
+                      _dealer!['profile_picture'],
+                      width: 120,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  _dealer!['full_name'],
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 12),
+                Container(
+                  padding: const EdgeInsets.all(12.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.greyBg),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          _dealer!['number_of_listings'].toString(),
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Text(
+                          "Number of Listings",
+                          style: TextStyle(color: AppColors.primary),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
